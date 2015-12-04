38e2aa9ef404c9a3cc647cd792cd463244daa7c7
sid802 <sid802@users.noreply.github.com>
2015-12-04 16:08:56 +0200

    updated to PEP-8 standards

--- phone_formats.py
+++ phone_formats.py
@@ -1,11 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-#################
+##################################
 #
-# phone
-# formats
+# phone_formats.py
+# contains phone formats
+# and creates appropriate regexes
 #
-#################
+##################################
 
 import re
 
@@ -36,26 +37,29 @@ class FormatList(list):
 		self.country_code = country_code
 
 	def copy(self):
-		return FormatList(self, country_code = self.country_code, strict = self._is_strict,
-						  canonized = self._is_canonized)
+		return FormatList(self, country_code=self.country_code, strict=self._is_strict,
+		                  canonized=self._is_canonized)
 
-	def to_find_regex(self, strict = False, country_code = '', optional_country = False, canonized = True, stuck_zero = False):
+	def to_find_regex(self, strict=False, country_code='', optional_country=False, canonized=True, stuck_zero=False):
 		"""
 		Creates a final compiled regex that will find phone numbers
 		"""
-		return self._create_full_regex(absolute_anchors = False, strict = strict, country_code = country_code, optional_country=optional_country,
-									   canonized = canonized, stuck_zero = stuck_zero)
+		return self._create_full_regex(absolute_anchors=False, strict=strict, country_code=country_code,
+		                               optional_country=optional_country,
+		                               canonized=canonized, stuck_zero=stuck_zero)
 
-	def to_exact_regex(self, strict = False, country_code = '', optional_country = False, canonized = True, stuck_zero = False):
+	def to_exact_regex(self, strict=False, country_code='', optional_country=False, canonized=True, stuck_zero=False):
 		"""
 		Creates a final compiled regex that will exactly match phone number (between anchors)
 		if strict or country_code are given as params, replace they are used instead of self.country_code and self._is_strict
 		"""
-		return self._create_full_regex(absolute_anchors = True, strict = strict, country_code = country_code, optional_country=optional_country,
-									   canonized = canonized, stuck_zero = stuck_zero)
+		return self._create_full_regex(absolute_anchors=True, strict=strict, country_code=country_code,
+		                               optional_country=optional_country,
+		                               canonized=canonized, stuck_zero=stuck_zero)
 
-	def _create_full_regex(self, absolute_anchors = True, strict = False, country_code = '', optional_country = False, canonized = True,
-						   stuck_zero = False):
+	def _create_full_regex(self, absolute_anchors=True, strict=False, country_code='', optional_country=False,
+	                       canonized=True,
+	                       stuck_zero=False):
 		"""
 		create full regex based on anchors.
 		absolute_anchors True: ^ and $
@@ -89,49 +93,49 @@ class FormatList(list):
 		if is_strict and country_code:  # with country code
 			if is_canonized:
 				final_regex = "{start_anchor}(?P<phone>0*{country}{alt}){end_anchor}".format(start_anchor=start_anchor,
-																					end_anchor=end_anchor,
-																					country=country_code,
-																					alt=orred_regexes)
+				                                                                             end_anchor=end_anchor,
+				                                                                             country=country_code,
+				                                                                             alt=orred_regexes)
 			else:
 				if not optional_country:
 					final_regex = "{start_anchor}(?P<phone>{country}{sep}{zero}{alt}){end_anchor}".format(
-						start_anchor = start_anchor,
-						end_anchor = end_anchor,
-						sep = separator,
-						country = country_code,
-						alt = orred_regexes,
-						zero = optional_zero)
+						start_anchor=start_anchor,
+						end_anchor=end_anchor,
+						sep=separator,
+						country=country_code,
+						alt=orred_regexes,
+						zero=optional_zero)
 				else:
 					final_regex = "{start_anchor}(?P<phone>0*({country}{sep})?{zero}{alt}){end_anchor}".format(
-						start_anchor = start_anchor,
-						end_anchor = end_anchor,
-						sep = separator,
-						country = country_code,
-						alt = orred_regexes,
-						zero = optional_zero)
+						start_anchor=start_anchor,
+						end_anchor=end_anchor,
+						sep=separator,
+						country=country_code,
+						alt=orred_regexes,
+						zero=optional_zero)
 		elif not country_code:
 			# No Country code given
-			final_regex = "{start_anchor}(?P<phone>0*({alt})){end_anchor}".format(start_anchor = start_anchor,
-																	   end_anchor = end_anchor,
-																	   country = country_code,
-																	   alt = orred_regexes)
+			final_regex = "{start_anchor}(?P<phone>0*({alt})){end_anchor}".format(start_anchor=start_anchor,
+			                                                                      end_anchor=end_anchor,
+			                                                                      country=country_code,
+			                                                                      alt=orred_regexes)
 		elif country_code:
 			# Country code given, but optional
 			final_regex = "{start_anchor}(?P<phone>(0*{country}{sep})?{zero}{alt}){end_anchor}".format(
-				start_anchor = start_anchor,
-				end_anchor = end_anchor,
-				country = country_code,
-				sep = separator,
-				alt = orred_regexes,
-				zero = optional_zero)
+				start_anchor=start_anchor,
+				end_anchor=end_anchor,
+				country=country_code,
+				sep=separator,
+				alt=orred_regexes,
+				zero=optional_zero)
 		else:
 			final_regex = "{start_anchor}(?P<phone>(0*{country}{sep})?{zero}{alt}){end_anchor}".format(
-				start_anchor = start_anchor,
-				end_anchor = end_anchor,
-				sep = separator,
-				country = country_code,
-				alt = orred_regexes,
-				zero = optional_zero)
+				start_anchor=start_anchor,
+				end_anchor=end_anchor,
+				sep=separator,
+				country=country_code,
+				alt=orred_regexes,
+				zero=optional_zero)
 
 		return re.compile(final_regex)
 
@@ -143,29 +147,30 @@ class FormatList(list):
 
 		basic_regexes = []
 
-		for index, format in enumerate(self):
+		for index, phone_format in enumerate(self):
 			current_regex = "(?P<num{i}>(\\d{sep1}){{{min},{max}}}\\d)0*{sep2}(?P<pre{i}>{prefix})".format(
-				prefix = format.prefix, sep1 = GENERAL_SEPARATOR, sep2 = OBLIGATED_SEPARATOR,
-				min = format.min_length - 1, max = format.max_length - 1, i = len(basic_regexes))
+				prefix=phone_format.prefix, sep1=GENERAL_SEPARATOR, sep2=OBLIGATED_SEPARATOR,
+				min=phone_format.min_length - 1, max=phone_format.max_length - 1, i=len(basic_regexes))
 			basic_regexes.append(current_regex)
 
 		return basic_regexes
 
-	def _create_regexes(self, canonized = True):
+	def _create_regexes(self, canonized=True):
 		"""
 		Create a regex for every phone format in the list
 		"""
 		basic_regexes = []
 
-		for format in self:
+		for phone_format in self:
 			if canonized:
-				current_regex = "{prefix}\\d{{{min},{max}}}".format(prefix = format.prefix, min = format.min_length,
-																	max = format.max_length)
+				current_regex = "{prefix}\\d{{{min},{max}}}".format(prefix=phone_format.prefix,
+				                                                          min=phone_format.min_length,
+				                                                          max=phone_format.max_length)
 			else:
-				current_regex = "{prefix}{sep}(\\d{sep}){{{min},{max}}}\\d".format(prefix = format.prefix,
-																				   sep = GENERAL_SEPARATOR,
-																				   min = format.min_length - 1,
-																				   max = format.max_length - 1)
+				current_regex = "{prefix}{sep}(\\d{sep}){{{min},{max}}}\\d".format(prefix=phone_format.prefix,
+				                                                                         sep=GENERAL_SEPARATOR,
+				                                                                         min=phone_format.min_length - 1,
+				                                                                         max=phone_format.max_length - 1)
 			basic_regexes.append(current_regex)
 
 		return basic_regexes
@@ -179,7 +184,7 @@ class FormatList(list):
 		regexes_orred = '|'.join(with_bracks)
 		return "({0})".format(regexes_orred)
 
-	def _create_orred_regexes(self, canonized = True):
+	def _create_orred_regexes(self, canonized=True):
 		"""
 		create regexes for all phone formats, and OR them in a big regex
 		"""
@@ -197,7 +202,7 @@ class FormatList(list):
 
 
 class Phone(object):
-	def __init__(self, comment = ''):
+	def __init__(self, comment=''):
 		self.formats = FormatList()
 		self.comment = comment
 
@@ -207,7 +212,6 @@ class Phone(object):
 		"""
 
 		self.formats.append(phone_format)
-
 
 	def _get_abs_min_max(self):
 		"""
@@ -219,13 +223,12 @@ class Phone(object):
 
 		return min(min_lengths), max(max_lengths)
 
-
-	def is_valid(self, phone_number, country_code = ''):
+	def is_valid(self, phone_number, country_code=''):
 		"""
 		Checks if phone_number matches any of the current formats.
 		if no country code is given, it doesnt look for any
 		"""
-		phone_regex = self.formats.to_exact_regex(country_code = country_code)
+		phone_regex = self.formats.to_exact_regex(country_code=country_code)
 		print phone_regex.pattern
 		match = re.search(phone_regex, phone_number)
 		if match:
@@ -240,8 +243,8 @@ class Phone(object):
 
 
 class CountryPhone(object):
-	def __init__(self, country = '', country_code = '', mobile_phone = Phone(), line_phone = Phone(),
-				 phones_dict = None):
+	def __init__(self, country='', country_code='', mobile_phone=Phone(), line_phone=Phone(),
+	             phones_dict=None):
 		self.country = country
 		self.country_code = country_code
 		if phones_dict:
@@ -263,51 +266,52 @@ class CountryPhone(object):
 
 		return min(mobile_min, line_min), max(mobile_max, line_max)
 
-	def is_valid_mobile(self, phone_number, strict = True):
+	def is_valid_mobile(self, phone_number, strict=True):
 		"""
 		checks if phone_number is a valid mobile. strict flag adds mobile country code
 		"""
 		if strict:
-			return self.mobile_phone.is_valid(phone_number = phone_number, country_code = self.country_code)
+			return self.mobile_phone.is_valid(phone_number=phone_number, country_code=self.country_code)
 		return self.mobile_phone.is_valid(phone_number)
 
-	def is_valid_line(self, phone_number, strict = True):
+	def is_valid_line(self, phone_number, strict=True):
 		"""
 		checks if phone_number is a valid line. strict flag adds line country code
 		"""
 		if strict:
-			return self.line_phone.is_valid(phone_number = phone_number, country_code = self.country_code)
+			return self.line_phone.is_valid(phone_number=phone_number, country_code=self.country_code)
 		return self.line_phone.is_valid(phone_number)
 
-	def is_valid(self, phone_number, strict = True):
+	def is_valid(self, phone_number, strict=True):
 		"""
 		checks if phone_number is a valid mobile or line number
 		"""
 		return self.is_valid_line(phone_number, strict) or self.is_valid_mobile(phone_number, strict)
 
-
-	def get_phone_formats(self, strict = True, canonized = True):
+	def get_phone_formats(self):
 		formats = self.mobile_phone.formats.copy()
 		formats.extend(self.line_phone.formats)
 		return formats
 
-	def to_find_regex(self, strict = False, with_country = True, optional_country = False, canonized = True, stuck_zero = False):
+	def to_find_regex(self, strict=False, with_country=True, optional_country=False, canonized=True, stuck_zero=False):
 		"""
 		Creates a final compiled regex that will find phone numbers
 		"""
-		return self._create_full_regex(absolute_anchors = False, strict = strict, with_country = with_country, optional_country = optional_country,
-									   canonized = canonized, stuck_zero = stuck_zero)
+		return self._create_full_regex(absolute_anchors=False, strict=strict, with_country=with_country,
+		                               optional_country=optional_country,
+		                               canonized=canonized, stuck_zero=stuck_zero)
 
-	def to_exact_regex(self, strict = False, with_country = True, optional_country = False, canonized = True, stuck_zero = False):
+	def to_exact_regex(self, strict=False, with_country=True, optional_country=False, canonized=True, stuck_zero=False):
 		"""
 		Creates a final compiled regex that will exactly match phone number (between anchors)
 		if strict or country_code are given as params, replace they are used instead of self.country_code and self._is_strict
 		"""
-		return self._create_full_regex(absolute_anchors = True, strict = strict, with_country = with_country, optional_country = optional_country,
-									   canonized = canonized, stuck_zero = stuck_zero)
+		return self._create_full_regex(absolute_anchors=True, strict=strict, with_country=with_country,
+		                               optional_country=optional_country,
+		                               canonized=canonized, stuck_zero=stuck_zero)
 
-	def _create_full_regex(self, absolute_anchors = True, strict = True, with_country = True, optional_country = False,
-						   canonized = True, stuck_zero = False):
+	def _create_full_regex(self, absolute_anchors=True, strict=True, with_country=True, optional_country=False,
+	                       canonized=True, stuck_zero=False):
 		"""
 		:param absolute_anchors:
 		:param strict:
@@ -318,26 +322,26 @@ class CountryPhone(object):
 		format_list = FormatList(self.get_phone_formats())
 
 		if with_country:
-			return format_list._create_full_regex(absolute_anchors = absolute_anchors,
-												  strict = strict,
-												  country_code = self.country_code,
-			                                      optional_country = optional_country,
-												  canonized = canonized,
-												  stuck_zero = stuck_zero)
+			return format_list._create_full_regex(absolute_anchors=absolute_anchors,
+			                                      strict=strict,
+			                                      country_code=self.country_code,
+			                                      optional_country=optional_country,
+			                                      canonized=canonized,
+			                                      stuck_zero=stuck_zero)
 
-		return format_list._create_full_regex(absolute_anchors = absolute_anchors,
-		                                      strict = strict,
+		return format_list._create_full_regex(absolute_anchors=absolute_anchors,
+		                                      strict=strict,
 		                                      optional_country=optional_country,
-											  canonized = canonized,
-		                                      stuck_zero = stuck_zero)
+		                                      canonized=canonized,
+		                                      stuck_zero=stuck_zero)
 
 	def __parse_phones_dict(self, phones_dict):
 		"""
 		Gets a dict of PhoneFormat instances. parses it to mobile and line phones_dict
 		return: tuple (mobile_phone, line_phone)
 		"""
-		mobile_phone = Phone(comment = "{country} - {kind} number".format(country = self.country, kind = "mobile"))
-		line_phone = Phone(comment = "{country} - {kind} number".format(country = self.country, kind = "line"))
+		mobile_phone = Phone(comment="{country} - {kind} number".format(country=self.country, kind="mobile"))
+		line_phone = Phone(comment="{country} - {kind} number".format(country=self.country, kind="line"))
 
 		for kind, phone_formats in phones_dict.iteritems():
 			for phone_format in phone_formats:
@@ -356,7 +360,7 @@ class CountryPhone(object):
 
 
 class PhoneFormat(object):
-	def __init__(self, prefix, min_length, max_length, comment = ''):
+	def __init__(self, prefix, min_length, max_length, comment=''):
 		"""
 		Creates a phone format. 
 		prefix: any regex
@@ -365,6 +369,7 @@ class PhoneFormat(object):
 		self.prefix = prefix
 		self.min_length = min_length
 		self.max_length = max_length
+		self.comment = comment
 
 
 def create_israeli_phone():
@@ -373,14 +378,14 @@ def create_israeli_phone():
 	"""
 	israeli_formats = {
 		'line': [
-			PhoneFormat(prefix = '[23489]', min_length = 7, max_length = 7, comment = r'geographic prefixes'),
-			PhoneFormat(prefix = '7', min_length = 8, max_length = 8, comment = r'non geographic Landline and VoIP')
+			PhoneFormat(prefix='[23489]', min_length=7, max_length=7, comment=r'geographic prefixes'),
+			PhoneFormat(prefix='7', min_length=8, max_length=8, comment=r'non geographic Landline and VoIP')
 		],
 
-		'mobile': [PhoneFormat(prefix = '5[^\\D]', min_length = 7, max_length = 7,
-							   comment = r'55 prefix is for virtual network operators')]
+		'mobile': [PhoneFormat(prefix='5[^\\D]', min_length=7, max_length=7,
+		                       comment=r'55 prefix is for virtual network operators')]
 	}
-	return CountryPhone(country = 'israel', country_code = '972', phones_dict = israeli_formats)
+	return CountryPhone(country='israel', country_code='972', phones_dict=israeli_formats)
 
 
 def create_belgian_phone():
@@ -389,15 +394,15 @@ def create_belgian_phone():
 	"""
 	belgian_formats = {
 		'line': [
-			PhoneFormat(prefix = '[^\\D0]', min_length = 7, max_length = 7, comment = r'geographic prefixes'),
-			PhoneFormat(prefix = '(70|78|90)', min_length = 6, max_length = 6,
-						comment = r'non geographic, mostly services'),
-			PhoneFormat(prefix = '800', min_length = 7, max_length = 7, comment = r'non geographic, mostly services')
+			PhoneFormat(prefix='[^\\D0]', min_length=7, max_length=7, comment=r'geographic prefixes'),
+			PhoneFormat(prefix='(70|78|90)', min_length=6, max_length=6,
+			            comment=r'non geographic, mostly services'),
+			PhoneFormat(prefix='800', min_length=7, max_length=7, comment=r'non geographic, mostly services')
 		],
 
-		'mobile': [PhoneFormat(prefix = '4[6789]', min_length = 7, max_length = 7)]
+		'mobile': [PhoneFormat(prefix='4[6789]', min_length=7, max_length=7)]
 	}
-	return CountryPhone(country = 'belgium', country_code = '32', phones_dict = belgian_formats)
+	return CountryPhone(country='belgium', country_code='32', phones_dict=belgian_formats)
 
 
 def create_holland_phone():
@@ -406,23 +411,21 @@ def create_holland_phone():
 	"""
 	france_formats = {
 		'line': [
-			PhoneFormat(prefix = '[12345]', min_length = 8, max_length = 8, comment = r'geographic prefixes'),
-			PhoneFormat(prefix = '[89]', min_length = 6, max_length = 6, comment = r'non geographic, mostly services')
+			PhoneFormat(prefix='[12345]', min_length=8, max_length=8, comment=r'geographic prefixes'),
+			PhoneFormat(prefix='[89]', min_length=6, max_length=6, comment=r'non geographic, mostly services')
 		],
 
-		'mobile': [PhoneFormat(prefix = '[67]', min_length = 8, max_length = 8)]
+		'mobile': [PhoneFormat(prefix='[67]', min_length=8, max_length=8)]
 	}
-	return CountryPhone(country = 'france', country_code = '33', phones_dict = france_formats)
+	return CountryPhone(country='france', country_code='33', phones_dict=france_formats)
+
 
 def create_all_phones():
 	"""
 	:return: dict of all phone formats
 	"""
 
-	phone_formats = {}
-
-	phone_formats['israel'] = create_israeli_phone()
-	phone_formats['belgium'] = create_belgian_phone()
-	phone_formats['holland'] = create_holland_phone()
-
+	phone_formats = {'israel': create_israeli_phone(), 'belgium': create_belgian_phone(),
+	                 'holland': create_holland_phone()}
+	
 	return phone_formats
